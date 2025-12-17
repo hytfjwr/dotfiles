54ef4ef5f1492b8612ac9e570a819aa027e1b487
hytfjwr <f.hayato0104@gmail.com>
2025-12-17 11:37:44 +0900

    Add local_bin scripts for environment setup

--- scripts/link.sh
+++ scripts/link.sh
@@ -20,6 +20,13 @@ ln -sfn "$DOTFILES_DIR/nvim" "$HOME/.config/nvim"
 echo "-> Linking karabiner"
 ln -sfn "$DOTFILES_DIR/karabiner" "$HOME/.config/karabiner"
 
+echo "-> Creating ~/.local/bin directory"
+mkdir -p "$HOME/.local/bin"
+
+echo "-> Linking local_bin scripts"
+ln -sfn "$DOTFILES_DIR/local_bin/env" "$HOME/.local/bin/env"
+ln -sfn "$DOTFILES_DIR/local_bin/env.fish" "$HOME/.local/bin/env.fish"
+
 # VSCode (macOS)
 VSCODE_DIR="$HOME/Library/Application Support/Code/User"
 if [ -d "$VSCODE_DIR" ]; then
